7e15810bdcaec642bf1d18cf08162c4f59dee7f3
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-06-29 15:34:05 +0000

    datatypes

---
+++ basics/01_variable.js
@@ -0,0 +1,24 @@
+//variable
+const accountId =144555773
+let accountEmail = "dasgopal@gmail.com"
+var accountpassword = "12345"
+acccoutCity = "Dantan"
+let accountState  //undefined
+
+
+ accountEmail = "87654hgfdethnbv34yuhb"
+ accountpassword = "98765refvcf"
+acccoutCity = "kolkata"
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountpassword)
+console.log(acccoutCity)
+
+/*
+prefer not to use var 
+because of issue in block scope and and function scope
+*/
+
+//table print
+console.table([accountId,accountEmail,accountpassword,acccoutCity,accountState])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3*3) // we are using nadejs, not browser
+
+// console.log(3+
+//     3); console.log("Gopal"); //code readability should be high
+
+let name = "Gopal"
+let age = 19
+let isLoggedIn = false
+let roll = null
+
+//number ==> 2 to power 53
+//bigint
+//string ==""
+//boolean =true / false
+//null => standlone value
+// undefined ==>
+// symbol => unique
+
+//object
+console.log(typeof name);
+console.log(typeof roll);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
