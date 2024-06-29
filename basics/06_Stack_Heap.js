135c20f7585fb68f417951e9632c7558e2871f8e
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-06-29 20:12:06 +0000

    Stack or heap

---
+++ basics/06_Stack_Heap.js
@@ -0,0 +1,16 @@
+//stack (primitive) , Heap (Non- primitive)
+
+let  myYoutubename = "@dasgopal13"
+
+
+//object
+let  userOne = {
+        email : "dasopal@0976afv",
+        upi: "721426"
+}
+
+let userTwo = userOne
+    userTwo.email = "sudipta@1234.com"
+
+    console.log(userOne.email);
+    console.log(userTwo.email);
