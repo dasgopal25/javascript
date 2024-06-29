4f9c702a1794f4290da18fe8ecd01ddef04743f2
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-06-29 18:45:28 +0000

    conversion of datatype

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = 33
+// console.log(typeof score)   //number
+// console.log(typeof(score))  //number
+// let valueInNumber = Number (score) 
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //33
+
+
+// let score = "33hsj"
+// console.log(typeof score)   //string
+// console.log(typeof(score))  //string
+// let valueInNumber = Number (score) 
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //NaN
+
+
+// let score = null
+// console.log(typeof score)  //object 
+// console.log(typeof(score))  //object
+// let valueInNumber = Number (score) 
+// console.log(typeof valueInNumber) //number
+// console.log(valueInNumber) //0
+
+// let isLoggeIn = "Gopal"
+// let booleanIsLoggIn = Boolean(isLoggeIn)
+// console.log(booleanIsLoggIn);//ture
+
+/* 1 => ture;0=>false
+ "" => false
+ "Gopal" => ture */
