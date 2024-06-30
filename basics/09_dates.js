32c86976b7a43342fa51e6d2174e625b2826e8ff
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-06-30 13:37:47 +0000

    date javascript

---
+++ basics/09_dates.js
@@ -0,0 +1,25 @@
+//Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatDate = new Date(2024,6,1,18,6)
+ // console.log(myCreatDate.toDateString());
+// console.log(myCreatDate.toLocaleString());
+
+// let myCreatDate = new Date("01-14-2023")
+// let myTimeStamp = Date.now()
+
+// console.log(myCreatDate)
+// console.log(myCreatDate.getTime())
+// console.log(Math.floor(Date.now()/1000)) //sec
+
+let myDate1 = new Date()
+console.log(myDate1);
+console.log(myDate1.getMonth());
+console.log(myDate1.getDay());
+
