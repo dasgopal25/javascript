d038cd46f0e2e74fdfcc13435408b9bc0986858d
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-07-01 19:44:05 +0000

    global or local scop

--- function/03_global_local_scop_in.js
+++ function/03_global_local_scop_in.js
@@ -1,1 +1,28 @@
-///
+// let a =300
+                    //global variables 
+// console.log(a)
+
+
+// if(true){
+//     let a =30
+//     const b = 30
+                       // local variables
+//     var c =50
+//     console.log(a)
+// }
+
+function one(){
+    const username = "Gopal"
+    
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    console.log(website);
+
+    two()
+}
+one()
+
+// ************hosting*******************
+
