69ec791f87a16c3d037e86a77dd496c374d0d233
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-07-01 14:44:57 +0000

    function part1

---
+++ function/01_function.js
@@ -0,0 +1,21 @@
+
+
+// function sayMyname() {
+//     console.log("g");
+//     console.log("o");
+//     console.log("p");
+//     console.log("a");
+//     console.log("l");  
+// }
+//  sayMyname()
+
+//  function addTwonumber(num1,num2){
+//     return num1+num2
+//  }
+//  const result = addTwonumber(5,6)
+//  console.log(`result is ${result}`);
+
+function loginUsersms(usename ="Gopal"){
+    return `${usename} just logged in`
+} 
+ console.log(loginUsersms())
