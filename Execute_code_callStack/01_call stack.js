25ec595efc6674496b31af319bd48d5000449d51
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-07-02 17:00:38 +0000

    call stack

---
+++ Execute_code_callStack/01_call stack.js
@@ -0,0 +1,9 @@
+/* javascript Execution Context
+Global  this EC
+1.global execution context
+2.Function execution context
+3.Eval execution context
+
+*Memory 'creation phase'. 
+**Execution phase.
+*/
