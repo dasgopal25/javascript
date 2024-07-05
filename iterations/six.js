7203333c1abe8d73b248812bcf32069f02e58691
dasgopal25 <164305686+dasgopal25@users.noreply.github.com>
2024-07-05 19:14:24 +0000

    dom

--- iterations/six.js
+++ iterations/six.js
@@ -8,7 +8,7 @@
 
 // console.log(values);
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
